6ce9d151fa5f488e858056e9db94e6b3d66ddc5f
Arkojyoti Sen <arkosen441@gmail.com>
2021-01-20 18:39:30 +0530

    Completed Basic Algorithms

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setWindowTitle("Arko's Grid");
+    w.show();
+    return a.exec();
+}
